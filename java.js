5d7f52dbc2dc34354fa4efc5239cfdcd2ca40634
VirsatusCodes <donavynpence@yahoo.com>
2022-06-06 20:50:28 -0700

    added onload to fill the array with local book, also linked up all text areas and buttons in my java and created my onload function.

--- java.js
+++ java.js
@@ -19,3 +19,38 @@ const eldest = new Book('Eldest', 'Christopher Paolini', 694, 5, 'read' );
 addBookToLibrary(eragon);
 
 addBookToLibrary(eldest);
+
+const bookNumber = document.querySelector('#book-number');
+
+const title = document.querySelector('#title');
+
+const author = document.querySelector('#author');
+
+const pages = document.querySelector('#pages');
+
+const score = document.querySelector('#score');
+
+const readOrNot = document.querySelector('#readOrNot');
+
+const backButton = document.querySelector('#back');
+
+const addButton = document.querySelector('#add');
+
+const readButton = document.querySelector('#read');
+
+const removeButton = document.querySelector('#remove');
+
+const nextButton = document.querySelector('#next');
+
+const searchPrompt = document.querySelector('#search-prompt');
+
+const searchButton = document.querySelector('#search');
+
+function onLoadFillTable(firstInArray) {
+    bookNumber.textContent = 1;
+    title.textContent = myLibrary[0].title;
+    author.textContent = myLibrary[0].author;
+    pages.textContent = myLibrary[0].pages;
+    score.textContent = myLibrary[0].score;
+    readOrNot.textContent = myLibrary[0].readOrNot;
+}
