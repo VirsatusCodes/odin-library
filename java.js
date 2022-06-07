c41f687d1dc3167cd76e120a50df7fd5b1afb5b0
VirsatusCodes <donavynpence@yahoo.com>
2022-06-06 19:46:21 -0700

    library, book creator & adder & test done

--- java.js
+++ java.js
@@ -1,1 +1,20 @@
-alert('hello');
+let myLibrary = [];
+
+function Book(title, author, oneToFive, readOrNot) {
+this.title=title;
+this.author=author;
+this.oneToFive=oneToFive;
+this.readOrNot=readOrNot;
+};
+
+function addBookToLibrary(book) {
+ myLibrary.push(book);
+};
+
+const eragon = new Book('Eragon', 'Christopher Paolini', 5, 'read' );
+
+const eldest = new Book('Eldest', 'Christopher Paolini', 5, 'read' );
+
+addBookToLibrary(eragon);
+
+addBookToLibrary(eldest);
