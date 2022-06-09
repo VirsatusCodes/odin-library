346c1e1b44ec9e1543d4f473ed97fd022680ae47
VirsatusCodes <donavynpence@yahoo.com>
2022-06-08 17:53:59 -0700

    basic book input setup and wired lota javascript did a bit of a reset and cleared stuff into a seperate doc for storage wired up my book input to add books to table on the fly SingleFillInRow, and fill table with in storage books fillTable, can also no make rows on the fly.

--- java.js
+++ java.js
@@ -1,6 +1,9 @@
 "use strict";
 
+
 let myLibrary = [];
+
+let recentBook=0
 
 function Book(title, author, pages, score, readOrNot) {
 this.title=title;
@@ -8,6 +11,7 @@ this.author=author;
 this.pages=pages;
 this.score=score;
 this.readOrNot=readOrNot;
+recentBook=this
 };
 
 function addBookToLibrary(book) {
@@ -16,9 +20,24 @@ function addBookToLibrary(book) {
 
 const eragon = new Book('Eragon', 'Christopher Paolini', 544, 5, 'read' );
 
+const farasd = new Book('fasrd', 'Christopher Paolini', 544, 5, 'read' );
+
+const fklnasdka = new Book ('dljknasla', 'ahsnidakda');
+
 addBookToLibrary(eragon);
 
-const bookNumber = document.querySelector('#book-number');
+addBookToLibrary(farasd);
+
+addBookToLibrary(fklnasdka);
+
+/* button.addeventlistener(blah blah() {
+    bring up prompt
+})
+
+button in prompt gets data for book then 
+you create the book and 
+ */
+const createButton = document.querySelector('#book-create');
 
 const title = document.querySelector('#title');
 
@@ -28,84 +47,58 @@ const pages = document.querySelector('#pages');
 
 const score = document.querySelector('#score');
 
-const readOrNot = document.querySelector('#readOrNot');
+const readOrNot = document.querySelector('#read-or-not');
 
-const addButton = document.querySelector('#add');
-
-const readButton = document.querySelector('#read');
-
-const removeButton = document.querySelector('#remove');
-
-const searchPrompt = document.querySelector('#search-prompt');
-
-const searchButton = document.querySelector('#search');
-
-let number = 0;
-function fillTable() {
-    bookNumber.textContent = number + 1;
-    title.textContent = myLibrary[number].title;
-    author.textContent = myLibrary[number].author;
-    pages.textContent = myLibrary[number].pages;
-    score.textContent = myLibrary[number].score;
-    readOrNot.textContent = myLibrary[number].readOrNot;
-};
-
-
-
-function checkIfEmpty(){
-/* if i were launching i would have this test if the user has a library
-yet and if they dont fill it with the ifEmpty info and if they do
-then fill it with the info they do have */
-};
-
-function ifEmpty(){
-    bookNumber.textContent = 'the library is...empty';
-    title.textContent = 'the library is...empty';
-    author.textContent = 'the library is...empty';
-    pages.textContent = 'the library is...empty';
-    score.textContent = 'the library is...empty';
-    readOrNot.textContent = 'you NEED to fix this : (';
-};
-
-addButton.addEventListener('click', () => {
-    const newBook = new Book (prompt());
-    addBookToLibrary(newBook);
-    fillTable();
-})
-
-readButton.addEventListener('click', () => {
-    if(readOrNot.textContent === 'read') readOrNot.textContent = 'not read... YET!';
-    else readOrNot.textContent = 'read';
-});
-
-removeButton.addEventListener('click', () =>{
-    if(myLibrary.length > 1){
-        myLibrary.splice(number, 1);
-        number= myLibrary.length -1;
-        fillTable();    
-    } else if (myLibrary.length === 1) {
-        myLibrary.splice(number, 1);
-        ifEmpty();
+createButton.addEventListener('click', () =>{ 
+    if(title.value != '' && author.value != '' && pages.value < 10000) {
+    new Book(title.value, author.value, pages.value, score.value, readOrNot.value);
+    addBookToLibrary(recentBook);
+    resetValues();
+    singleFillInRow();
     } else return;
 });
 
+function resetValues(){
+    title.value='';
+    author.value='';
+    pages.value='';
+    score.value='';
+    readOrNot.value='';
+}
 
-const theTable = document.querySelector('table');
-const tableRow = document.createElement('tr');
-const tableData = document.createElement('td');
-const newTableRow = document.querySelector('table:last-child');
-const newtable = theTable.lastChild;
-const anotherTable = document.querySelector('tbody');
-const selected = anotherTable.lastChild;
+const tableRows = document.querySelector('tbody');
 
-anotherTable.appendChild(tableRow);
-anotherTable.lastChild.appendChild(tableData);
-tableData.textContent='test';
-const tableData2 = document.createElement('td');
-tableData2.textContent= 'other test';
-anotherTable.lastChild.appendChild(tableData2);
-
-function showBook() {
+let lastTableRow = 0;
+let i = 0
+function makeRow() {
     const tableRow = document.createElement('tr');
+    tableRows.appendChild(tableRow);
+    for (i = 0; i < 5 ; i++ ){
     const tableData = document.createElement('td');
+    tableRows.lastChild.appendChild(tableData);
+    }
+    lastTableRow = tableRows.lastChild
+}
+
+
+
+
+function fillTable() {
+    for(let i = 0; i < myLibrary.length ; i++){
+        makeRow();
+        lastTableRow.childNodes[0].textContent=myLibrary[i].title;
+        lastTableRow.childNodes[1].textContent=myLibrary[i].author;
+        lastTableRow.childNodes[2].textContent=myLibrary[i].pages;
+        lastTableRow.childNodes[3].textContent=myLibrary[i].score;
+        lastTableRow.childNodes[4].textContent=myLibrary[i].readOrNot; 
+    };
+};
+
+function singleFillInRow() {
+    makeRow();
+    lastTableRow.childNodes[0].textContent=myLibrary[myLibrary.length-1].title;
+    lastTableRow.childNodes[1].textContent=myLibrary[myLibrary.length-1].author;
+    lastTableRow.childNodes[2].textContent=myLibrary[myLibrary.length-1].pages;
+    lastTableRow.childNodes[3].textContent=myLibrary[myLibrary.length-1].score;
+    lastTableRow.childNodes[4].textContent=myLibrary[myLibrary.length-1].readOrNot; 
 }
